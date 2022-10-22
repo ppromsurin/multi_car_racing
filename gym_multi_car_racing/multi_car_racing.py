8799551f844c8721835f06a53c5dd7a64a796319
Phil Promsurin <ppromsurin@ufl.edu>
2022-10-21 23:12:14 -0400

    added position and playfield to env.step output to prevent unknown early termination, added future angles to state output in env.step

--- gym_multi_car_racing/multi_car_racing.py
+++ gym_multi_car_racing/multi_car_racing.py
@@ -473,9 +473,10 @@ class MultiCarRacing(gym.Env, EzPickle):
         # self.state = self.render("state_pixels") # MY CODE
 
         step_reward = np.zeros(self.num_agents)
-        state_vec = np.zeros((self.num_agents, 3))
+        state_vec = np.zeros((self.num_agents, 13))
 
         done = False
+        terminate = False
         if action is not None: # First step without action, called from reset()
             self.reward -= 0.1
             # We actually don't want to count fuel spent, we want car to be faster.
@@ -572,8 +573,23 @@ class MultiCarRacing(gym.Env, EzPickle):
                 angle_diff2 = desired_angle - car_angle
                 angle_diff2 = (angle_diff2 + (2 * np.pi)) % (2 * np.pi)
 
+                # get track angles for track after current
+                future_track_idx = np.array(np.arange(track_index, track_index+10) % len(self.track), dtype=int)
+                # print(future_track_idx)
+                # print(type(future_track_idx))
+                fucknumpy = np.asarray(self.track, dtype=object) # I fucking hate numpy
+                # print(type(self.track))
+                # print(fucknumpy.shape)
+                # print(fucknumpy[future_track_idx])
+                future_angles = fucknumpy[future_track_idx,1]
+                future_angles = (future_angles + (2*np.pi)) % (2* np.pi)
+                des_future_angles = future_angles - car_angle
+                # print(future_angles)
+                print(des_future_angles)
+
+
                 speed = np.linalg.norm(vel)
-                state_vec[car_id] = [speed, centerline_distance, angle_diff2]
+                state_vec[car_id] = np.concatenate(([speed, centerline_distance, angle_diff2], des_future_angles),axis = None)
 
                 # Reward function for going off track
                 border = TRACK_WIDTH/2
@@ -594,9 +610,19 @@ class MultiCarRacing(gym.Env, EzPickle):
                 x, y = car.hull.position
                 if abs(x) > PLAYFIELD or abs(y) > PLAYFIELD:
                     done = True
+                    terminate = True
+                    print(x)
+                    print(y)
                     step_reward[car_id] = -100
         self.state = state_vec
-        return self.state, step_reward, done, {}
+        if done:
+            print('Done')
+
+        info = {
+            "position": car.hull.position,
+            "playfield": PLAYFIELD
+        }
+        return self.state, step_reward, done, info
 
     def render(self, mode='human', viewMe = 'car'):
         assert mode in ['human', 'state_pixels', 'rgb_array']
@@ -698,6 +724,7 @@ class MultiCarRacing(gym.Env, EzPickle):
         if None not in self.viewer:
             for viewer in self.viewer:
                 viewer.close()
+                print('why')
 
         self.viewer = [None] * self.num_agents
 
@@ -765,65 +792,65 @@ class MultiCarRacing(gym.Env, EzPickle):
                                  ('c3B', (0, 0, 255) * 3))
 
 
-if __name__=="__main__":
-    from pyglet.window import key
-    NUM_CARS = 2  # Supports key control of two cars, but can simulate as many as needed
-
-    # Specify key controls for cars
-    CAR_CONTROL_KEYS = [[key.LEFT, key.RIGHT, key.UP, key.DOWN],
-                        [key.A, key.D, key.W, key.S]]
-
-    a = np.zeros((NUM_CARS,3))
-    def key_press(k, mod):
-        global restart, stopped, CAR_CONTROL_KEYS
-        if k==0xff1b: stopped = True # Terminate on esc.
-        if k==0xff0d: restart = True # Restart on Enter.
-
-        # Iterate through cars and assign them control keys (mod num controllers)
-        for i in range(min(len(CAR_CONTROL_KEYS), NUM_CARS)):
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][0]:  a[i][0] = -1.0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][1]: a[i][0] = +1.0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][2]:    a[i][1] = +1.0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][3]:  a[i][2] = +0.8   # set 1.0 for wheels to block to zero rotation
-
-    def key_release(k, mod):
-        global CAR_CONTROL_KEYS
-
-        # Iterate through cars and assign them control keys (mod num controllers)
-        for i in range(min(len(CAR_CONTROL_KEYS), NUM_CARS)):
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][0]  and a[i][0]==-1.0: a[i][0] = 0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][1] and a[i][0]==+1.0: a[i][0] = 0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][2]:    a[i][1] = 0
-            if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][3]:  a[i][2] = 0
-
-
-    env = MultiCarRacing(NUM_CARS)
-    env.render()
-    for viewer in env.viewer:
-        viewer.window.on_key_press = key_press
-        viewer.window.on_key_release = key_release
-    record_video = False
-    if record_video:
-        from gym.wrappers.monitor import Monitor
-        env = Monitor(env, '/tmp/video-test', force=True)
-    isopen = True
-    stopped = False
-    while isopen and not stopped:
-        env.reset()
-        total_reward = np.zeros(NUM_CARS)
-        steps = 0
-        restart = False
-        while True:
-            s, r, done, info = env.step(a)
-            total_reward += r
-            if steps % 200 == 0 or done:
-                print("\nActions: " + str.join(" ", [f"Car {x}: "+str(a[x]) for x in range(NUM_CARS)]))
-                print(f"Step {steps} Total_reward "+str(total_reward))
-                #import matplotlib.pyplot as plt
-                #plt.imshow(s)
-                #plt.savefig("test.jpeg")
-            steps += 1
-            isopen = env.render().all()
-            if stopped or done or restart or isopen == False:
-                break
-    env.close()
+# if __name__=="__main__":
+#     from pyglet.window import key
+#     NUM_CARS = 2  # Supports key control of two cars, but can simulate as many as needed
+# 
+#     # Specify key controls for cars
+#     CAR_CONTROL_KEYS = [[key.LEFT, key.RIGHT, key.UP, key.DOWN],
+#                         [key.A, key.D, key.W, key.S]]
+# 
+#     a = np.zeros((NUM_CARS,3))
+#     def key_press(k, mod):
+#         global restart, stopped, CAR_CONTROL_KEYS
+#         if k==0xff1b: stopped = True # Terminate on esc.
+#         if k==0xff0d: restart = True # Restart on Enter.
+# 
+#         # Iterate through cars and assign them control keys (mod num controllers)
+#         for i in range(min(len(CAR_CONTROL_KEYS), NUM_CARS)):
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][0]:  a[i][0] = -1.0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][1]: a[i][0] = +1.0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][2]:    a[i][1] = +1.0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][3]:  a[i][2] = +0.8   # set 1.0 for wheels to block to zero rotation
+# 
+#     def key_release(k, mod):
+#         global CAR_CONTROL_KEYS
+# 
+#         # Iterate through cars and assign them control keys (mod num controllers)
+#         for i in range(min(len(CAR_CONTROL_KEYS), NUM_CARS)):
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][0]  and a[i][0]==-1.0: a[i][0] = 0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][1] and a[i][0]==+1.0: a[i][0] = 0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][2]:    a[i][1] = 0
+#             if k==CAR_CONTROL_KEYS[i % len(CAR_CONTROL_KEYS)][3]:  a[i][2] = 0
+# 
+# 
+#     env = MultiCarRacing(NUM_CARS)
+#     env.render()
+#     for viewer in env.viewer:
+#         viewer.window.on_key_press = key_press
+#         viewer.window.on_key_release = key_release
+#     record_video = False
+#     if record_video:
+#         from gym.wrappers.monitor import Monitor
+#         env = Monitor(env, '/tmp/video-test', force=True)
+#     isopen = True
+#     stopped = False
+#     while isopen and not stopped:
+#         env.reset()
+#         total_reward = np.zeros(NUM_CARS)
+#         steps = 0
+#         restart = False
+#         while True:
+#             s, r, done, info = env.step(a)
+#             total_reward += r
+#             if steps % 200 == 0 or done:
+#                 print("\nActions: " + str.join(" ", [f"Car {x}: "+str(a[x]) for x in range(NUM_CARS)]))
+#                 print(f"Step {steps} Total_reward "+str(total_reward))
+#                 #import matplotlib.pyplot as plt
+#                 #plt.imshow(s)
+#                 #plt.savefig("test.jpeg")
+#             steps += 1
+#             isopen = env.render().all()
+#             if stopped or done or restart or isopen == False:
+#                 break
+#     env.close()
